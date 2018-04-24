0cb00b271a42a6e49843a04bfa6d222604cb7b7a
Keyrat06 <raoulemil@gmail.com>
2018-04-24 12:40:14 -0400

    did some work using the new data

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
+
 
 plt.ion()
+
 
 NUM_WORLDS = 200
 KEEP_PROP = 0.6
 NUM_PLAYERS = 7
 NUM_PERIODS = 12
-GENERATIONS = 1000
+GENERATIONS = 200
 
 a = pd.read_excel("Demand_Data.xls")
 b = pd.read_excel("Company_Data.xls", range(NUM_PLAYERS))
-types = ["Bay View", "Big Coal", "Fossil Light","Beachfront", "Tobin", "Big Gas", "East Bay"]
+types = ["Bay View", "Big Coal", "Fossil Light", "Beachfront", "Old Timers", "Big Gas", "East Bay"]
 demands = a.demand
 colors = ['dimgray','c','g','y','r','b','m']
 
 
 class GeneticPlayer:
-    def __init__(self, capacity, var_cost, OM, num_periods=12, name="", r=0.25, s=0.5):
+    def __init__(self, capacity, var_cost, OM, num_periods=12, name="", names=[], r=0.25, s=0.5):
         self.name = name
+        self.names = names
         self.num_periods = num_periods
         self.N = len(capacity)
         self.capacity = capacity
@@ -67,10 +71,13 @@ def play_game(players):
 
 
 def mix_players(player_1, player_2):
-    new_player = GeneticPlayer(player_1.capacity, player_1.MC, player_1.OM, player_1.num_periods, player_1.name)
+    new_player = GeneticPlayer(player_1.capacity, player_1.MC, player_1.OM,
+                               player_1.num_periods, player_1.name, player_1.names)
     for i in range(len(new_player.bids)):
         for j in range(len(new_player.bids[0])):
             new_player.bids[i, j] = np.random.choice((player_1, player_2)).bids[i, j]
+            if np.random.random() < 0.05:
+                new_player.bids[i, j] = player_1.MC[i]*(1+np.random.normal(0, 0.5)**2)
     return new_player
 
 
@@ -81,7 +88,8 @@ def get_pop_types():
         OM = sum(b[i].OM)
         capacity = tuple(b[i].Capacity)
         var_cost = tuple(b[i].Total_Var_Cost)
-        data.append({"name": name, "OM": OM, "capacity": capacity, "var_cost": var_cost})
+        names = tuple(b[i].Parts)
+        data.append({"name": name, "OM": OM, "capacity": capacity, "var_cost": var_cost, "names": names})
     return data
 
 
@@ -101,6 +109,7 @@ def make_worlds(num_worlds):
         worlds.append(make_world())
     return worlds
 
+
 def evaluate_worlds(worlds):
     scores = np.zeros((len(worlds), NUM_PLAYERS))
     for i, world in enumerate(worlds):
@@ -109,7 +118,7 @@ def evaluate_worlds(worlds):
 
 
 def kill_weak(worlds, scores, keep_num=4):
-    order = np.argsort(-1*scores/np.sum(scores, axis=1)[:,np.newaxis], axis=0) #np.argsort(-1*scores/np.sum(scores, axis=1)[:,np.newaxis], axis=0)
+    order = np.argsort(-1*scores/np.sum(scores, axis=1)[:, np.newaxis], axis=0) #np.argsort(-1*scores/np.sum(scores, axis=1)[:,np.newaxis], axis=0)
     new_worlds = np.empty((keep_num, NUM_PLAYERS), dtype=worlds.dtype)
     for i in range(keep_num):
         for j in range(NUM_PLAYERS):
@@ -125,7 +134,7 @@ def kill_weak(worlds, scores, keep_num=4):
 def populate(worlds, N):
     new_worlds = list(worlds)
     while len(new_worlds) < N:
-        if np.random.random() < 0.05:
+        if np.random.random() < 0.025:
             new_worlds.append(make_world())
         else:
             world = []
@@ -137,14 +146,12 @@ def populate(worlds, N):
     return new_worlds
 
 
-
-
-
 def make_simple_world():
     players = []
     for pop_type in pop_types:
         players.append(GeneticPlayer(r=0, s=0, **pop_type))
     return players
+
 
 simple_world = make_simple_world()
 base_score = list(play_game(simple_world))
@@ -167,10 +174,30 @@ def simulate():
             plt.plot([], c=colors[p], label=types[p])
         plt.plot([], c='k', linestyle='-', label="Genetic")
         plt.plot([], c='k', linestyle='--', label="bid = MC")
+        plt.title("Average Total Profit at Each Generation")
         plt.xlabel("Generation")
-        plt.ylabel("Total Profit")
+        plt.ylabel("Average Total Profit")
         plt.legend()
         plt.pause(0.001)
     print np.mean(scores, axis=0)
+    plt.show()
+    scores = evaluate_worlds(worlds)
+    worlds = kill_weak(worlds, scores,1)
+    return worlds[0]
 
-simulate()
+def flatten_world(world, period=0):
+    data = []
+    for porfolio in world:
+        name = porfolio.name
+        for j, part in enumerate(porfolio.names):
+            bid = porfolio.bids[j, period]
+            capacity = porfolio.capacity[j]
+            data.append((bid, capacity, part, types.index(name)))
+    return data
+
+if __name__ == "__main__":
+    world = simulate()
+    flat_world = flatten_world(world)
+    pickle.dump(flat_world, open("world.p", 'w'))
+
+
---
+++ visualize.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+import numpy as np
+import pickle
+
+NUM_PLAYERS = 7
+
+a = pd.read_excel("Demand_Data.xls")
+b = pd.read_excel("Company_Data.xls", range(NUM_PLAYERS))
+types = ["Bay View", "Big Coal", "Fossil Light", "Beachfront", "Old Timers", "Big Gas", "East Bay"]
+demands = a.demand
+colors = ['dimgray', 'c', 'g', 'y', 'r', 'b', 'm']
+
+
+def get_all_data():
+    data = [] ## [(var_cost, capacity, plant_name, portfolioname), (var_cost, capacity, plant_name, portfolioname), ...]
+    for i in range(NUM_PLAYERS):
+        name = str(types[i])
+        for j, part in enumerate(b[i].Parts):
+            capacity = float(b[i].Capacity[j])
+            var_cost = float(b[i].Total_Var_Cost[j])
+            data.append((var_cost, capacity, str(part), types.index(name)))
+    return sorted(data)
+
+bid_mc = get_all_data()
+
+def visualize(data, show=True, period=0):
+    cumulative = 0
+    data = sorted(data)
+    for datapoint in data:
+        plt.plot((cumulative, cumulative+datapoint[1]), (datapoint[0], datapoint[0]), color=colors[datapoint[3]])
+        plt.text(cumulative, datapoint[0] + 1, datapoint[2], fontsize=5)
+        cumulative += datapoint[1]
+    for i, type in enumerate(types):
+        plt.plot([], label=type, color=colors[i])
+
+
+    plt.vlines(demands[period], 0, 100)
+    plt.text(demands[period]-5, 5, "week {} hour {}".format(int(period)//4 + 1, period%4 + 1), fontsize=5)
+    plt.legend()
+
+    if show:
+        plt.show()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        visualize(bid_mc, True, 0)
+    elif len(sys.argv) == 2:
+        period = int(sys.argv[1])
+        visualize(bid_mc, True, period)
+    else:
+        world = pickle.load(open(sys.argv[1]))
+        period = int(sys.argv[2])
+        visualize(world, True, period)
+
